0b20d29c09e90d4f7a58424f943ab81bc2eab4af
Gabriel Aguiar <72907265+Raiugami@users.noreply.github.com>
2021-07-23 22:47:55 -0300

    Add files via upload

---
+++ 1001.py
@@ -0,0 +1,6 @@
+A: str = input()
+B: str = input()
+
+X = int (A) + int (B)
+
+print("X = {}".format (X))
---
+++ 1010.py
@@ -0,0 +1,10 @@
+codum = int (input()) ,
+quantum = int(input())
+valorum = float (input())
+coddois = int (input())
+quantdois = int(input())
+valordois = float (input())
+
+resultado = (quantum * valorum) + (quantdois * valordois)
+
+print("VALOR A PAGAR: R$ {:.2f}" .format(resultado))
